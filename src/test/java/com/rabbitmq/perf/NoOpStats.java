efac118e09a8abaf7a53dc1ef70b0beccddb58bb
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2018-09-10 18:04:00 +0200

    Add missing class

---
+++ src/test/java/com/rabbitmq/perf/NoOpStats.java
@@ -0,0 +1,33 @@
+// Copyright (c) 2018-Present Pivotal Software, Inc.  All rights reserved.
+//
+// This software, the RabbitMQ Java client library, is triple-licensed under the
+// Mozilla Public License 1.1 ("MPL"), the GNU General Public License version 2
+// ("GPL") and the Apache License version 2 ("ASL"). For the MPL, please see
+// LICENSE-MPL-RabbitMQ. For the GPL, please see LICENSE-GPL2.  For the ASL,
+// please see LICENSE-APACHE2.
+//
+// This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND,
+// either express or implied. See the LICENSE file for specific language governing
+// rights and limitations of this software.
+//
+// If you have any questions regarding licensing, please contact us at
+// info@rabbitmq.com.
+
+package com.rabbitmq.perf;
+
+import io.micrometer.core.instrument.composite.CompositeMeterRegistry;
+
+/**
+ *
+ */
+public class NoOpStats extends Stats {
+
+    public NoOpStats() {
+        super(1000, false, new CompositeMeterRegistry(), "");
+    }
+
+    @Override
+    protected void report(long now) {
+
+    }
+}
