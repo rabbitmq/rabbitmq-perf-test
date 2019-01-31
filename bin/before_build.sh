331c6be929f2b1d130cfd29d6805108daef0ad33
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2019-01-31 10:49:15 +0100

    Fix Travis CI
    Broker is installed in a custom script, not in the Travis CI
    configuration file.

--- bin/before_build.sh
+++ bin/before_build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.9/rabbitmq-server-generic-unix-3.7.9.tar.xz
-tar xf rabbitmq-server-generic-unix-3.7.9.tar.xz
-mv rabbitmq_server-3.7.9 rabbitmq
+wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.10/rabbitmq-server-generic-unix-3.7.10.tar.xz
+tar xf rabbitmq-server-generic-unix-3.7.10.tar.xz
+mv rabbitmq_server-3.7.10 rabbitmq
 
 rabbitmq/sbin/rabbitmq-server -detached
 
