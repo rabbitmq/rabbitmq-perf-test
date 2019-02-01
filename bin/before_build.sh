74679b2d692db9807ca522bb3c7a476392d81021
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2019-02-01 13:38:31 +0100

    Bump RabbitMQ to 3.7.11 on Travis CI

--- bin/before_build.sh
+++ bin/before_build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.10/rabbitmq-server-generic-unix-3.7.10.tar.xz
-tar xf rabbitmq-server-generic-unix-3.7.10.tar.xz
-mv rabbitmq_server-3.7.10 rabbitmq
+wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.11/rabbitmq-server-generic-unix-3.7.11.tar.xz
+tar xf rabbitmq-server-generic-unix-3.7.11.tar.xz
+mv rabbitmq_server-3.7.11 rabbitmq
 
 rabbitmq/sbin/rabbitmq-server -detached
 
