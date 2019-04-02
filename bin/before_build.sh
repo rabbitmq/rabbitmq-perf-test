f3729c00cdcc3851ef990fe907a5bfc031b820fb
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2019-04-02 09:51:44 +0200

    Bump RabbitMQ to 3.7.14 on Travis CI

--- bin/before_build.sh
+++ bin/before_build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.13/rabbitmq-server-generic-unix-3.7.13.tar.xz
-tar xf rabbitmq-server-generic-unix-3.7.13.tar.xz
-mv rabbitmq_server-3.7.13 rabbitmq
+wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.7.14/rabbitmq-server-generic-unix-3.7.14.tar.xz
+tar xf rabbitmq-server-generic-unix-3.7.14.tar.xz
+mv rabbitmq_server-3.7.14 rabbitmq
 
 rabbitmq/sbin/rabbitmq-server -detached
 
