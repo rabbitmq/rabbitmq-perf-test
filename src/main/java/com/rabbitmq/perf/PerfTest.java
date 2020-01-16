baf266aefaa945cabdd7bd45c160ff47d3eaeb8d
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2020-01-16 10:24:42 +0100

    Remove test line
    References #231

--- src/main/java/com/rabbitmq/perf/PerfTest.java
+++ src/main/java/com/rabbitmq/perf/PerfTest.java
@@ -45,7 +45,6 @@ public class PerfTest {
     private static final Logger LOGGER = LoggerFactory.getLogger(PerfTest.class);
 
     public static void main(String [] args, PerfTestOptions perfTestOptions) {
-        args = "-r 100 --consumer-args x-priority=10,foo=bar".split(" ");
         SystemExiter systemExiter = perfTestOptions.systemExiter;
         ShutdownService shutdownService = perfTestOptions.shutdownService;
         Options options = getOptions();
