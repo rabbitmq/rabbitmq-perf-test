1dae9727c308f364fbdf817105b90443826fa24f
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2020-07-16 14:47:43 +0200

    Bump Erlang and RabbitMQ in Travis CI

--- bin/before_build.sh
+++ bin/before_build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.0/rabbitmq-server-generic-unix-3.8.0.tar.xz
-tar xf rabbitmq-server-generic-unix-3.8.0.tar.xz
-mv rabbitmq_server-3.8.0 rabbitmq
+wget https://github.com/rabbitmq/rabbitmq-server/releases/download/v3.8.5/rabbitmq-server-generic-unix-3.8.5.tar.xz
+tar xf rabbitmq-server-generic-unix-3.8.5.tar.xz
+mv rabbitmq_server-3.8.5 rabbitmq
 
 rabbitmq/sbin/rabbitmq-server -detached
 
