0c38774a99d9a21c1f1ecf1507970923a6bab26f
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2023-01-04 09:25:16 +0100

    Revert "Revert "Start metrics service before consumers and publishers""
    This reverts commit 3043cdfd17e71b73e00e23eeb76ade031393328b.

--- src/test/java/com/rabbitmq/perf/metrics/DefaultPerformanceMetricsTest.java
+++ src/test/java/com/rabbitmq/perf/metrics/DefaultPerformanceMetricsTest.java
@@ -166,6 +166,7 @@ public class DefaultPerformanceMetricsTest {
 
   void execute(Configurator configurator) {
     DefaultPerformanceMetrics metrics = metrics(configurator);
+    metrics.started(true);
     metrics.metrics(System.nanoTime());
     this.defaultOutput = defaultConsoleOut.toString();
     this.compactOutput = compactConsoleOut.toString();
