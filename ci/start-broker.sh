7d1f6ae8839f11a986c730f85ead69e841ac2a90
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2023-03-08 16:46:20 +0100

    Use matrix to test against broker alphas

--- ci/start-broker.sh
+++ ci/start-broker.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-RABBITMQ_IMAGE_TAG=${RABBITMQ_IMAGE_TAG:-3.11}
-RABBITMQ_IMAGE=${RABBITMQ_IMAGE:-rabbitmq}
+RABBITMQ_IMAGE=${RABBITMQ_IMAGE:-rabbitmq:3.11}
 
 wait_for_message() {
   while ! docker logs "$1" | grep -q "$2";
@@ -11,12 +10,12 @@ wait_for_message() {
   done
 }
 
-echo "Running RabbitMQ ${RABBITMQ_IMAGE}:${RABBITMQ_IMAGE_TAG}"
+echo "Running RabbitMQ ${RABBITMQ_IMAGE}"
 
 docker rm -f rabbitmq 2>/dev/null || echo "rabbitmq was not running"
 docker run -d --name rabbitmq \
     --network host \
-    "${RABBITMQ_IMAGE}":"${RABBITMQ_IMAGE_TAG}"
+    "${RABBITMQ_IMAGE}"
 
 wait_for_message rabbitmq "completed with"
 
