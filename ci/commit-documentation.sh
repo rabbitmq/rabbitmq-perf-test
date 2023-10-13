3b60ed844237cccaa25ad056a11696ec02f3f22e
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2023-10-13 15:32:56 +0200

    Remove GH pages and deployment to CF worker
    Using CF pages now.

--- ci/commit-documentation.sh
+++ ci/commit-documentation.sh
@@ -5,27 +5,26 @@ POM_VERSION=$(cat pom.xml | grep -oPm1 '(?<=<version>)[^<]+')
 # shellcheck disable=SC2102
 if [[ $POM_VERSION == *[SNAPSHOT]* ]]
 then
-  DIRECTORY="snapshot"
+  PAGES_BRANCH="pages-snapshot"
 elif [[ $POM_VERSION == *[RCM]* ]]; then
-  DIRECTORY="milestone"
+  PAGES_BRANCH="pages-milestone"
 else
-  DIRECTORY="release"
+  PAGES_BRANCH="pages-release"
 fi
 
 MESSAGE=$(git log -1 --pretty=%B)
 
 # Concourse does shallow clones, so need the next 2 commands to have the gh-pages branch
-git remote set-branches origin 'gh-pages'
+git remote set-branches origin $PAGES_BRANCH
 git fetch -v
 git checkout gh-pages
-mkdir -p $DIRECTORY/htmlsingle
-cp target/generated-docs/index.html $DIRECTORY/htmlsingle
+cp target/generated-docs/index.html .
 if [ -z "$(git status --porcelain)" ];
 then
   echo "Nothing to commit"
   git checkout main
 else
-  git add $DIRECTORY/
+  git add .
   git commit -m "$MESSAGE"
   git push origin gh-pages
   git checkout main
